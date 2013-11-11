8242ccc7a787026851abe05f99d9404df30d3788
ziolo90211 <ziolo90211@wp.pl>
2013-11-11 04:25:50 +0100

    first commit

---
+++ sql/base/auth.sql
@@ -0,0 +1,411 @@
+/*
+BLIZZQL File Transfer
+
+Source Server         : eu.battle.net
+Source Server Version :1100019231
+Source Host           : eu.battle.net:2709113
+Source Database       : authentification
+
+Target Server Type    : BLIZZQL
+Target Server Version : 1100019231
+File Encoding         : 65001
+
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `account`
+-- ----------------------------
+DROP TABLE IF EXISTS `account`;
+CREATE TABLE `account` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Identifier',
+  `username` varchar(32) NOT NULL DEFAULT '',
+  `sha_pass_hash` varchar(40) NOT NULL DEFAULT '',
+  `sessionkey` varchar(80) NOT NULL DEFAULT '',
+  `v` varchar(64) NOT NULL DEFAULT '',
+  `s` varchar(64) NOT NULL DEFAULT '',
+  `email` varchar(254) NOT NULL DEFAULT '',
+  `joindate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_ip` varchar(15) NOT NULL DEFAULT '127.0.0.1',
+  `failed_logins` int(10) unsigned NOT NULL DEFAULT '0',
+  `locked` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `lock_country` varchar(2) NOT NULL DEFAULT '00',
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `online` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `expansion` tinyint(4) unsigned NOT NULL DEFAULT '4',
+  `mutetime` bigint(20) NOT NULL DEFAULT '0',
+  `mutereason` varchar(255) NOT NULL DEFAULT '',
+  `muteby` varchar(50) NOT NULL DEFAULT '',
+  `locale` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `os` varchar(3) NOT NULL DEFAULT '',
+  `recruiter` int(10) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COMMENT='Account System';
+
+-- ----------------------------
+-- Records of account
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `account_access`
+-- ----------------------------
+DROP TABLE IF EXISTS `account_access`;
+CREATE TABLE `account_access` (
+  `id` int(10) unsigned NOT NULL,
+  `gmlevel` tinyint(3) unsigned NOT NULL,
+  `RealmID` int(11) NOT NULL DEFAULT '-1',
+  PRIMARY KEY (`id`,`RealmID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of account_access
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `account_banned`
+-- ----------------------------
+DROP TABLE IF EXISTS `account_banned`;
+CREATE TABLE `account_banned` (
+  `id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Account id',
+  `bandate` int(10) unsigned NOT NULL DEFAULT '0',
+  `unbandate` int(10) unsigned NOT NULL DEFAULT '0',
+  `bannedby` varchar(50) NOT NULL,
+  `banreason` varchar(255) NOT NULL,
+  `active` tinyint(3) unsigned NOT NULL DEFAULT '1',
+  PRIMARY KEY (`id`,`bandate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Ban List';
+
+-- ----------------------------
+-- Records of account_banned
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `autobroadcast`
+-- ----------------------------
+DROP TABLE IF EXISTS `autobroadcast`;
+CREATE TABLE `autobroadcast` (
+  `realmid` int(11) NOT NULL DEFAULT '-1',
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  `weight` tinyint(3) unsigned DEFAULT '1',
+  `text` longtext NOT NULL,
+  PRIMARY KEY (`id`,`realmid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of autobroadcast
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `ip2nation`
+-- ----------------------------
+DROP TABLE IF EXISTS `ip2nation`;
+CREATE TABLE `ip2nation` (
+  `ip` int(11) unsigned NOT NULL DEFAULT '0',
+  `country` char(2) NOT NULL DEFAULT '',
+  KEY `ip` (`ip`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of ip2nation
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `ip2nationcountries`
+-- ----------------------------
+DROP TABLE IF EXISTS `ip2nationcountries`;
+CREATE TABLE `ip2nationcountries` (
+  `code` varchar(4) NOT NULL DEFAULT '',
+  `iso_code_2` varchar(2) NOT NULL DEFAULT '',
+  `iso_code_3` varchar(3) DEFAULT '',
+  `iso_country` varchar(255) NOT NULL DEFAULT '',
+  `country` varchar(255) NOT NULL DEFAULT '',
+  `lat` float NOT NULL DEFAULT '0',
+  `lon` float NOT NULL DEFAULT '0',
+  PRIMARY KEY (`code`),
+  KEY `code` (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of ip2nationcountries
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `ip_banned`
+-- ----------------------------
+DROP TABLE IF EXISTS `ip_banned`;
+CREATE TABLE `ip_banned` (
+  `ip` varchar(15) NOT NULL DEFAULT '127.0.0.1',
+  `bandate` int(10) unsigned NOT NULL,
+  `unbandate` int(10) unsigned NOT NULL,
+  `bannedby` varchar(50) NOT NULL DEFAULT '[Console]',
+  `banreason` varchar(255) NOT NULL DEFAULT 'no reason',
+  PRIMARY KEY (`ip`,`bandate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Banned IPs';
+
+-- ----------------------------
+-- Records of ip_banned
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `logs`
+-- ----------------------------
+DROP TABLE IF EXISTS `logs`;
+CREATE TABLE `logs` (
+  `time` int(10) unsigned NOT NULL,
+  `realm` int(10) unsigned NOT NULL,
+  `type` tinyint(3) unsigned NOT NULL,
+  `level` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `string` text CHARACTER SET latin1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of logs
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `rbac_account_groups`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_account_groups`;
+CREATE TABLE `rbac_account_groups` (
+  `accountId` int(10) unsigned NOT NULL COMMENT 'Account id',
+  `groupId` int(10) unsigned NOT NULL COMMENT 'Group id',
+  `realmId` int(11) NOT NULL DEFAULT '-1' COMMENT 'Realm Id, -1 means all',
+  PRIMARY KEY (`accountId`,`groupId`,`realmId`),
+  KEY `fk__rbac_account_groups__rbac_groups` (`groupId`),
+  CONSTRAINT `fk__rbac_account_groups__account` FOREIGN KEY (`accountId`) REFERENCES `account` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk__rbac_account_groups__rbac_groups` FOREIGN KEY (`groupId`) REFERENCES `rbac_groups` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Account-Group relation';
+
+-- ----------------------------
+-- Records of rbac_account_groups
+-- ----------------------------
+INSERT INTO `rbac_account_groups` VALUES ('1', '1', '-1');
+INSERT INTO `rbac_account_groups` VALUES ('2', '1', '-1');
+INSERT INTO `rbac_account_groups` VALUES ('3', '1', '-1');
+INSERT INTO `rbac_account_groups` VALUES ('4', '1', '-1');
+INSERT INTO `rbac_account_groups` VALUES ('5', '1', '-1');
+INSERT INTO `rbac_account_groups` VALUES ('6', '1', '-1');
+INSERT INTO `rbac_account_groups` VALUES ('2', '2', '-1');
+INSERT INTO `rbac_account_groups` VALUES ('2', '3', '-1');
+INSERT INTO `rbac_account_groups` VALUES ('2', '4', '-1');
+
+-- ----------------------------
+-- Table structure for `rbac_account_permissions`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_account_permissions`;
+CREATE TABLE `rbac_account_permissions` (
+  `accountId` int(10) unsigned NOT NULL COMMENT 'Account id',
+  `permissionId` int(10) unsigned NOT NULL COMMENT 'Permission id',
+  `granted` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Granted = 1, Denied = 0',
+  `realmId` int(11) NOT NULL DEFAULT '-1' COMMENT 'Realm Id, -1 means all',
+  PRIMARY KEY (`accountId`,`permissionId`,`realmId`),
+  KEY `fk__rbac_account_roles__rbac_permissions` (`permissionId`),
+  CONSTRAINT `fk__rbac_account_permissions__account` FOREIGN KEY (`accountId`) REFERENCES `account` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk__rbac_account_roles__rbac_permissions` FOREIGN KEY (`permissionId`) REFERENCES `rbac_permissions` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Account-Permission relation';
+
+-- ----------------------------
+-- Records of rbac_account_permissions
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `rbac_account_roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_account_roles`;
+CREATE TABLE `rbac_account_roles` (
+  `accountId` int(10) unsigned NOT NULL COMMENT 'Account id',
+  `roleId` int(10) unsigned NOT NULL COMMENT 'Role id',
+  `granted` tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Granted = 1, Denied = 0',
+  `realmId` int(11) NOT NULL DEFAULT '-1' COMMENT 'Realm Id, -1 means all',
+  PRIMARY KEY (`accountId`,`roleId`,`realmId`),
+  KEY `fk__rbac_account_roles__rbac_roles` (`roleId`),
+  CONSTRAINT `fk__rbac_account_roles__account` FOREIGN KEY (`accountId`) REFERENCES `account` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk__rbac_account_roles__rbac_roles` FOREIGN KEY (`roleId`) REFERENCES `rbac_roles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Account-Role relation';
+
+-- ----------------------------
+-- Records of rbac_account_roles
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `rbac_groups`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_groups`;
+CREATE TABLE `rbac_groups` (
+  `id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Group id',
+  `name` varchar(100) NOT NULL COMMENT 'Group name',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Group List';
+
+-- ----------------------------
+-- Records of rbac_groups
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `rbac_group_roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_group_roles`;
+CREATE TABLE `rbac_group_roles` (
+  `groupId` int(10) unsigned NOT NULL COMMENT 'group id',
+  `roleId` int(10) unsigned NOT NULL COMMENT 'Role id',
+  PRIMARY KEY (`groupId`,`roleId`),
+  KEY `fk__rbac_group_roles__rbac_roles` (`roleId`),
+  CONSTRAINT `fk__rbac_group_roles__rbac_groups` FOREIGN KEY (`groupId`) REFERENCES `rbac_groups` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk__rbac_group_roles__rbac_roles` FOREIGN KEY (`roleId`) REFERENCES `rbac_roles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Group Role relation';
+
+-- ----------------------------
+-- Records of rbac_group_roles
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `rbac_permissions`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_permissions`;
+CREATE TABLE `rbac_permissions` (
+  `id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Permission id',
+  `name` varchar(100) NOT NULL COMMENT 'Permission name',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Permission List';
+
+-- ----------------------------
+-- Records of rbac_permissions
+-- ----------------------------
+-- ----------------------------
+-- Table structure for `rbac_roles`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_roles`;
+CREATE TABLE `rbac_roles` (
+  `id` int(10) unsigned NOT NULL DEFAULT '0' COMMENT 'Role id',
+  `name` varchar(100) NOT NULL COMMENT 'Role name',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Roles List';
+
+-- ----------------------------
+-- Records of rbac_roles
+-- ----------------------------
+-- ----------------------------
+-- Table structure for `rbac_role_permissions`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_role_permissions`;
+CREATE TABLE `rbac_role_permissions` (
+  `roleId` int(10) unsigned NOT NULL COMMENT 'Role id',
+  `permissionId` int(10) unsigned NOT NULL COMMENT 'Permission id',
+  PRIMARY KEY (`roleId`,`permissionId`),
+  KEY `fk__role_permissions__rbac_permissions` (`permissionId`),
+  CONSTRAINT `fk__role_permissions__rbac_permissions` FOREIGN KEY (`permissionId`) REFERENCES `rbac_permissions` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk__role_permissions__rbac_roles` FOREIGN KEY (`roleId`) REFERENCES `rbac_roles` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Role Permission relation';
+
+-- ----------------------------
+-- Records of rbac_role_permissions
+-- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for `rbac_security_level_groups`
+-- ----------------------------
+DROP TABLE IF EXISTS `rbac_security_level_groups`;
+CREATE TABLE `rbac_security_level_groups` (
+  `secId` int(10) unsigned NOT NULL COMMENT 'Security Level id',
+  `groupId` int(10) unsigned NOT NULL COMMENT 'group id',
+  PRIMARY KEY (`secId`,`groupId`),
+  KEY `fk__rbac_security_level_groups__rbac_groups` (`groupId`),
+  CONSTRAINT `fk__rbac_security_level_groups__rbac_groups` FOREIGN KEY (`groupId`) REFERENCES `rbac_groups` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Default groups to assign when an account is set gm level';
+
+-- ----------------------------
+-- Records of rbac_security_level_groups
+-- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for `realmcharacters`
+-- ----------------------------
+DROP TABLE IF EXISTS `realmcharacters`;
+CREATE TABLE `realmcharacters` (
+  `realmid` int(10) unsigned NOT NULL DEFAULT '0',
+  `acctid` int(10) unsigned NOT NULL,
+  `numchars` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`realmid`,`acctid`),
+  KEY `acctid` (`acctid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Realm Character Tracker';
+
+-- ----------------------------
+-- Records of realmcharacters
+-- ----------------------------
+
+
+-- ----------------------------
+-- Table structure for `realmlist`
+-- ----------------------------
+DROP TABLE IF EXISTS `realmlist`;
+CREATE TABLE `realmlist` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) NOT NULL DEFAULT '',
+  `address` varchar(255) NOT NULL DEFAULT '127.0.0.1',
+  `localAddress` varchar(255) NOT NULL DEFAULT '127.0.0.1',
+  `localSubnetMask` varchar(255) NOT NULL DEFAULT '255.255.255.0',
+  `port` smallint(5) unsigned NOT NULL DEFAULT '8085',
+  `icon` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `flag` tinyint(3) unsigned NOT NULL DEFAULT '2',
+  `timezone` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `allowedSecurityLevel` tinyint(3) unsigned NOT NULL DEFAULT '0',
+  `population` float unsigned NOT NULL DEFAULT '0',
+  `gamebuild` int(10) unsigned NOT NULL DEFAULT '17128',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Realm System';
+
+-- ----------------------------
+-- Records of realmlist
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `realm_classes`
+-- ----------------------------
+DROP TABLE IF EXISTS `realm_classes`;
+CREATE TABLE `realm_classes` (
+  `realmId` int(11) NOT NULL,
+  `class` tinyint(4) NOT NULL COMMENT 'Class Id',
+  `expansion` tinyint(4) NOT NULL COMMENT 'Expansion for class activation',
+  PRIMARY KEY (`realmId`,`class`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of realm_classes
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `realm_races`
+-- ----------------------------
+DROP TABLE IF EXISTS `realm_races`;
+CREATE TABLE `realm_races` (
+  `realmId` int(11) NOT NULL,
+  `race` tinyint(4) NOT NULL COMMENT 'Race Id',
+  `expansion` tinyint(4) NOT NULL COMMENT 'Expansion for race activation',
+  PRIMARY KEY (`realmId`,`race`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of realm_races
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for `uptime`
+-- ----------------------------
+DROP TABLE IF EXISTS `uptime`;
+CREATE TABLE `uptime` (
+  `realmid` int(10) unsigned NOT NULL,
+  `starttime` int(10) unsigned NOT NULL DEFAULT '0',
+  `uptime` int(10) unsigned NOT NULL DEFAULT '0',
+  `maxplayers` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `revision` varchar(255) NOT NULL DEFAULT 'Trinitycore',
+  PRIMARY KEY (`realmid`,`starttime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Uptime system';
+
+-- ----------------------------
+-- Records of uptime
+-- ----------------------------
